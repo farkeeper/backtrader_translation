c357c67b8f37f3b89ac60c33110986c25603ef82
平底鞋 <farserver@163.com>
2023-02-26 19:52:18 +0800

    Update __init__.py
    导包和导模块

--- backtrader/__init__.py
+++ backtrader/__init__.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; py-indent-offset:4 -*-
 # ##############################################################################
-# 将有用的模块导入到__init__模块里，此后可通过backtrader.module调用
+# 将有用的模块导入到__init__模块里，此后可通过backtrader.模块名调用
 # 有__init__模块的文件夹，叫 包，.py结尾的文件叫模块
-# 如果把包看作一个类，那么，__init__就像类的初始化函数，__init__内的代码就像类的属性
+# 如果把包看作一个类，那么，__init__就像类的初始化函数，__init__内的代码就像类的属性，被导入的模块就像函数的具体实现
 # __init__ 就是包的初始化， __init__ 内的各种类、函数、常(变)量，就是包的属性
 # 类.属性名 可以调用类的属性，包.属性名 就可以调用该包的各种属性（模块、类、函数、常(变)量）
 # ##############################################################################
 """
     把有用的模块导入到 __init__模块里
-    以后用 backtrader.module 就能调用该模块了
+    以后用 backtrader.模块名 就能调用该模块了
 """
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-# from .模块名 ：导入同级模块
+# 从同级模块中导入变量
 from .version import __version__, __btversion__
 
 from .errors import *
@@ -58,7 +58,7 @@ from .cerebro import *
 from .timer import *
 from .flt import *
 
-# from . import 同级文件夹
+# 导入同级别的包
 from . import utils as utils
 
 from . import feeds as feeds
